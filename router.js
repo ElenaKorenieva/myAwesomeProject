fbde48bc30d48a04df03dbc6a0ebe6f8ffbfec95
Olena Korenieva <olena.korenieva@gmail.com>
2023-09-25 19:51:48 +0200

    Fixed router.js

--- router.js
+++ router.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import LoginScreen from "./Screens/auth/LoginScreen.jsx";
 import RegistrationScreen from "./Screens/auth/RegistrationScreen.jsx";
-import PostsScreen from "./Screens/mainScreen/PostsScreen.jsx";
-import CreateScreen from "./Screens/mainScreen/CreatePostsScreen.jsx";
-import ProfileScreen from "./Screens/mainScreen/ProfileScreen.jsx";
 
 const AuthStack = createStackNavigator();
-const MainTab = createBottomTabNavigator();
 
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
 import { Home } from "./Screens/home/Home.jsx";
 
 export const useRoute = (isAuth) => {
@@ -37,14 +30,14 @@ export const useRoute = (isAuth) => {
     );
   }
   return (
-    <MainTab.Navigator tabBarOptions={{ showLabel: false }}>
-      <MainTab.Screen
+    <AuthStack.Navigator tabBarOptions={{ showLabel: false }}>
+      <AuthStack.Screen
         name="Home"
         component={Home}
         options={{
           headerShown: false,
         }}
       />
-    </MainTab.Navigator>
+    </AuthStack.Navigator>
   );
 };
